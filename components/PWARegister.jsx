80ed1f33cbf46a37b34c8c72d9e0d282e978f55c
quehometech-cell <quehometech@gmail.com>
2026-09-26 16:50:13 -0400

    Register PWA service worker

---
+++ components/PWARegister.jsx
@@ -0,0 +1,3 @@
+"use client";
+import { useEffect } from "react";
+export default function PWARegister(){useEffect(()=>{if("serviceWorker" in navigator){navigator.serviceWorker.register("/sw.js").catch((error)=>console.error("Service worker registration failed:",error));}},[]);return null;}
